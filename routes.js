d978f7d4d8c866ebf0ec1366728149aa4b727766
gabriel <gabriel.lucas.asr@gmail.com>
2020-07-02 23:29:17 -0300

    teacher creation page

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const teachers = require('./teachers')
 const routes = express.Router()
 
 routes.get('/', function(req, res) {
@@ -12,5 +13,7 @@ routes.get('/teachers', function(req, res) {
 routes.get('/teachers/create', function (req, res) {
     return res.render('teachers/create')
 })
+
+routes.post('/teachers', teachers.post)
 
 module.exports = routes
